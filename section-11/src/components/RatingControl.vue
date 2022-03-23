9aa96b14714bf235766210c1159174305d5809c1
Rutvik Patel <rutvikpatel90990@gmail.com>
2022-03-23 09:55:33 +0530

    add section-11

---
+++ section-11/src/components/RatingControl.vue
@@ -0,0 +1,64 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+        <li :class="{active: modelValue === 'average'}"><button type="button" @click="activate('average')">Average</button></li>
+        <li :class="{active: modelValue === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props : ['modelValue'],
+    emits : ['update:modelValue'],
+    // data() {
+    //     return {
+    //         activeOption: this.modelValue
+    //     }
+    // },
+    computed: {
+    //    activeOption(){
+    //        this.modelValue
+    //    }
+    },
+    methods: {
+        activate(option){
+          
+          this.$emit('update:modelValue',option);
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.active{
+    border-color: #a00078;
+}
+
+.active button{
+    border-color: #a00078;
+}
+ul{
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0.5rem;
+    display: flex;
+}
+
+li{
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+button{
+    font:inherit;
+    border: 1px;
+    background-color:transparent;
+    cursor: pointer;
+}
+</style>
